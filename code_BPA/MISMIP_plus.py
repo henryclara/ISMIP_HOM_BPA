7bb7a0525088ae59ce168c59d8de11469edac090
henryclara <clara.henry@bas.ac.uk>
2026-05-18 10:53:36 +0100

    Halve the domain due to symmetry

--- code_BPA/MISMIP_plus.py
+++ code_BPA/MISMIP_plus.py
@@ -3,12 +3,15 @@ import numpy as np
 import time
 
 Lx = 640000.0
-Ly = 80000.0
-nx = 160
-ny = 20
+Ly_full = 80000.0
+Ly = Ly_full / 2.0
+
+nx = 640
+ny = 40
 nz = 10
 
 base = RectangleMesh(nx, ny, Lx, Ly)
+base.coordinates.dat.data[:, 1] += Ly_full / 2.0
 mesh = ExtrudedMesh(base, layers=nz, layer_height=1.0 / nz)
 x, y, sigma = SpatialCoordinate(mesh)
 
@@ -73,7 +76,7 @@ dc = Constant(500.0)
 zdeep = Constant(-720.0)
 
 def mismip_bed(x, y):
-    yc = y - Ly / 2.0
+    yc = y - Ly_full / 2.0
     X = x / xbar
 
     Bx = B0 + B2 * X**2 + B4 * X**4 + B6 * X**6
@@ -112,10 +115,10 @@ beta2.interpolate(Constant(1.0e4))
 a_s = Constant(0.3)
 a_b = Constant(0.0)
 
-dts = [1, 5, 10]
-theta_outs = [1, 0]
-FSSA_keywords = ["approx", "full", None]
-T = 2000.0
+dts = [0.5]
+theta_outs = [0]
+FSSA_keyword = "full"
+T = 10000.0
 
 # For theta_out == 0
 bcs_u = [
@@ -143,188 +146,219 @@ bcs_ubar = [
     DirichletBC(VVbar.sub(1), 0.0, 4),
 ]
 
+delta_zb = Constant(1.0)
+grounded_out = Function(Vbar, name="grounded")
+
+def reset_state():
+    # Reset geometry
+    zb.interpolate(max_value(-90.0, bed))
+    thick.interpolate(100.0)
+    zs.interpolate(zb + thick)
+
+    mesh.coordinates.interpolate(
+        as_vector([xref, yref, zb + sigmaref * thick])
+    )
+
+    # Reset solution fields
+    w.assign(0.0)
+    uvec_out.assign(0.0)
+    H.assign(0.0)
+    u_prev.assign(0.0)
+
+    # Reset velocity guess
+    u_init = as_vector([
+        10.0 * (1.0 + 0.01 * sin(2.0 * pi * x / Lx) * sin(2.0 * pi * y / Ly_full)),
+        10.0 * (0.01 * sin(2.0 * pi * x / Lx) * sin(2.0 * pi * y / Ly_full)),
+    ])
+
+    w.sub(0).interpolate(u_init)
+    w.sub(1).interpolate(u_init)
+
+    u0 = w.sub(0)
+    ux0, uy0 = split(u0)
+    w.sub(2).project(thick * (ux0.dx(0) + uy0.dx(1)))
+
+def save_restart(filename, t, step, dt, theta_out):
+    with CheckpointFile(filename, "w") as afile:
+        afile.save_mesh(mesh)
+        afile.save_function(thick, name="thick")
+        afile.save_function(zb, name="zb")
+        afile.save_function(zs, name="zs")
+        afile.save_function(w, name="w")
+        afile.save_function(uvec_out, name="uvec_out")
+
+    np.savez(filename.replace(".h5", "_meta.npz"), t=t, step=step, dt=dt, theta_out=theta_out)
+
 for dt in dts:
     for theta_out in theta_outs:
-        for FSSA_keyword in FSSA_keywords:
-            print("=" * 80)
-            print(f"Starting run with dt={dt:g}, theta_out={theta_out:g}")
-            print("=" * 80)
 
-            theta = Constant(theta_out)
-            num_TS = int(T / dt)
-                    
-            mesh.coordinates.interpolate(as_vector([xref, yref, zb + sigmaref * thick]))
+        reset_state()
+        
+        print("=" * 80)
+        print(f"Starting run with dt={dt:g}, theta_out={theta_out:g}")
+        print("=" * 80)
 
-            u_init = as_vector([10.0 * (1.0 + 0.01 * sin(2.0*pi*x/Lx) * sin(2.0*pi*y/Ly)), \
-                                10.0 * (0.01 * sin(2.0*pi*x/Lx) * sin(2.0*pi*y/Ly)),])
+        theta = Constant(theta_out)
+        num_TS = int(T / dt)
 
-            w.sub(0).interpolate(u_init)
-            w.sub(1).interpolate(u_init)
+        outfile = VTKFile(f"Simulations/MISMIP_output_theta{theta_out:g}_dt{dt:g}.pvd")
 
-            u0 = w.sub(0)
-            ux0, uy0 = split(u0)
+        for i in range(num_TS):
+            for j, n in enumerate(ns):
+                print("Solving with n = ", n)
 
-            w.sub(2).project(thick * (ux0.dx(0) + uy0.dx(1)))
+                if theta_out == 0:
+                    uvec = Function(VV)
+                    uvec.interpolate(w.sub(0))
 
-            u_prev.assign(0.0)
+                    ux, uy = split(uvec)
 
-            outfile = VTKFile(f"Simulations/MISMIP_output_{FSSA_keyword}_theta{theta_out:g}_dt{dt:g}.pvd")
+                    du = TrialFunction(VV)
+                    vvect = TestFunction(VV)
+                    v1, v2 = split(vvect)
 
-            for i in range(num_TS):
-                for j, n in enumerate(ns):
-                    print("Solving with n = ", n)
+                else:
+                    uvec, u_s, q = split(w)
+                    ux, uy = split(uvec)
+                    ux_s, uy_s = split(u_s)
 
-                    if theta == 0:
-                        uvec = Function(VV)
-                        uvec.interpolate(w.sub(0))
+                    dw = TrialFunction(W)
+                    vvect, eta, r = TestFunctions(W)
+                    v1, v2 = split(vvect)
 
-                        ux, uy = split(uvec)
+                mu = viscosity(ux, uy, n)
 
-                        du = TrialFunction(VV)
-                        vvect = TestFunction(VV)
-                        v1, v2 = split(vvect)
+                surf = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
 
-                    else:
-                        uvec, u_s, q = split(w)
-                        ux, uy = split(uvec)
-                        ux_s, uy_s = split(u_s)
+                F = (4 * mu * ux.dx(0) + 2 * mu * uy.dx(1)) * v1.dx(0) * dx \
+                                + (mu * ux.dx(1) + mu * uy.dx(0)) * v1.dx(1) * dx \
+                                + mu * ux.dx(2) * v1.dx(2) * dx
+                            
+                F += (4 * mu * uy.dx(1) + 2 * mu * ux.dx(0)) * v2.dx(1) * dx \
+                                + (mu * uy.dx(0) + mu * ux.dx(1)) * v2.dx(0) * dx \
+                                + mu * uy.dx(2) * v2.dx(2) * dx
 
-                        dw = TrialFunction(W)
-                        vvect, eta, r = TestFunctions(W)
-                        v1, v2 = split(vvect)
+                #delta_zb = Constant(1.0)
+                grounded = 1.0 - tanh((zb - bed) / delta_zb)
+                zeta = Constant(1.0) - grounded
 
-                    mu = viscosity(ux, uy, n)
+                F += grounded * beta2 * dot(uvec, vvect) * ds_b
 
-                    surf = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
+                F -= rhoi * g * zs * (v1.dx(0) + v2.dx(1)) * dx
+                
+                if theta_out != 0 and FSSA_keyword == "full":
+                    F += theta * rhoi * g * dt * (ux_s * zs.dx(0) + uy_s * zs.dx(1)) * (v1.dx(0) + v2.dx(1)) * dx
+                    F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * q * (v1.dx(0) + v2.dx(1)) * dx
+                    F += 0.5 * theta * rhoi * g * dt * (ux * (zs * zs).dx(0) + uy * (zs * zs).dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
+                    F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * zs * (v1.dx(0) + v2.dx(1)) * surf * q * ds_t
+                
+                    # The constraints:
+                    F += dot(u_s - uvec, eta) * ds_t
+                    F += r * q * dx - r * thick * (ux.dx(0) + uy.dx(1)) * dx
 
-                    F = (4 * mu * ux.dx(0) + 2 * mu * uy.dx(1)) * v1.dx(0) * dx \
-                                    + (mu * ux.dx(1) + mu * uy.dx(0)) * v1.dx(1) * dx \
-                                    + mu * ux.dx(2) * v1.dx(2) * dx
-                                
-                    F += (4 * mu * uy.dx(1) + 2 * mu * ux.dx(0)) * v2.dx(1) * dx \
-                                    + (mu * uy.dx(0) + mu * ux.dx(1)) * v2.dx(0) * dx \
-                                    + mu * uy.dx(2) * v2.dx(2) * dx
+                    # The accumulation terms:
+                    n_z = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
+                    F -= theta * a_s * n_z * rhoi * g * dt * (zs.dx(0) * v1 + zs.dx(1) * v2) * ds_t
+                    F -= theta * rhoi * g * dt * a_s * (v1.dx(0) + v2.dx(1)) * dx
 
-                    delta_zb = Constant(1.0)
-                    grounded = 1.0 - tanh((zb - bed) / delta_zb)
-                    zeta = Constant(1.0) - grounded
+                # The stabilisation terms (the version where we assume that test function gradients are depth independent)
+                if theta_out != 0 and FSSA_keyword == "approx":
+                    # This terms needs to be updated
+                    F += theta * rhoi * g * dt * thick * (ux * zs.dx(0) + uy * zs.dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
+                    F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * thick * (ux.dx(0) + uy.dx(1)) * (v1.dx(0) + v2.dx(1)) * dx
+                    F += 0.5 * theta * rhoi * g * dt * (ux * (zs * zs).dx(0) + uy * (zs * zs).dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
+                    F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * zs * (v1.dx(0) + v2.dx(1)) * surf * thick * (ux.dx(0) + uy.dx(1)) * ds_t
 
-                    F += grounded * beta2 * dot(uvec, vvect) * ds_b
+                    # The accumulation terms:
+                    n_z = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
+                    F -= theta * a_s * n_z * rhoi * g * dt * (zs.dx(0) * v1 + zs.dx(1) * v2) * ds_t
+                    F -= theta * rhoi * g * dt * a_s * (v1.dx(0) + v2.dx(1)) * dx
 
-                    F -= rhoi * g * zs * (v1.dx(0) + v2.dx(1)) * dx
-                    
-                    if theta != 0 and FSSA_keyword == "full":
-                        F += theta * rhoi * g * dt * (ux_s * zs.dx(0) + uy_s * zs.dx(1)) * (v1.dx(0) + v2.dx(1)) * dx
-                        F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * q * (v1.dx(0) + v2.dx(1)) * dx
-                        F += 0.5 * theta * rhoi * g * dt * (ux * (zs * zs).dx(0) + uy * (zs * zs).dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
-                        F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * zs * (v1.dx(0) + v2.dx(1)) * surf * q * ds_t
-                    
-                        # The constraints:
-                        F += dot(u_s - uvec, eta) * ds_t
-                        F += r * q * dx - r * thick * (ux.dx(0) + uy.dx(1)) * dx
+                z = SpatialCoordinate(mesh)[2]
+                sea_level = Constant(0.0)
 
-                        # The accumulation terms:
-                        n_z = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
-                        F -= theta * a_s * n_z * rhoi * g * dt * (zs.dx(0) * v1 + zs.dx(1) * v2) * ds_t
-                        F -= theta * rhoi * g * dt * a_s * (v1.dx(0) + v2.dx(1)) * dx
+                p_ocean = rhow * g * max_value(sea_level - z, 0.0)
 
-                    # The stabilisation terms (the version where we assume that test function gradients are depth independent)
-                    if theta_out != 0 and FSSA_keyword == "approx":
-                        # This terms needs to be updated
-                        F += theta * rhoi * g * dt * thick * (ux * zs.dx(0) + uy * zs.dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
-                        F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * thick * (ux.dx(0) + uy.dx(1)) * (v1.dx(0) + v2.dx(1)) * dx
-                        F += 0.5 * theta * rhoi * g * dt * (ux * (zs * zs).dx(0) + uy * (zs * zs).dx(1)) * (v1.dx(0) + v2.dx(1)) * surf * ds_t
-                        F += theta * rhoi * g * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * zs * (v1.dx(0) + v2.dx(1)) * surf * thick * (ux.dx(0) + uy.dx(1)) * ds_t
+                # To do: Get the ocean pressure properly set up. The current version is close, but not strictly accurate
+                #F += p_ocean * v1 * ds_v(2)
 
-                        # The accumulation terms:
-                        n_z = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
-                        F -= theta * a_s * n_z * rhoi * g * dt * (zs.dx(0) * v1 + zs.dx(1) * v2) * ds_t
-                        F -= theta * rhoi * g * dt * a_s * (v1.dx(0) + v2.dx(1)) * dx
+                if theta_out == 0:
+                    J = derivative(F, uvec, du)
+                    problem = NonlinearVariationalProblem(F, uvec, bcs=bcs_u, J=J)
+                else:
+                    J = derivative(F, w, dw)
+                    problem = NonlinearVariationalProblem(F, w, bcs=bcs_w, J=J)
 
-                    z = SpatialCoordinate(mesh)[2]
-                    sea_level = Constant(0.0)
+                solver = NonlinearVariationalSolver(
+                    problem,
+                    solver_parameters={
+                        "snes_type": "newtonls",
+                        "snes_linesearch_type": "bt",
+                        "snes_rtol": 1.0e-5,
+                        "snes_atol": 1.0e-5,
+                        "snes_max_it": 200,
 
-                    p_ocean = rhow * g * max_value(sea_level - z, 0.0)
+                        "mat_type": "aij",
+                        "ksp_type": "preonly",
+                        "pc_type": "lu",
+                        "pc_factor_mat_solver_type": "mumps",
 
-                    # To do: Get the ocean pressure properly set up. The current version is close, but not strictly accurate
-                    #F += p_ocean * v1 * ds_v(2)
-
-                    if theta == 0:
-                        J = derivative(F, uvec, du)
-                        problem = NonlinearVariationalProblem(F, uvec, bcs=bcs_u, J=J)
-                    else:
-                        J = derivative(F, w, dw)
-                        problem = NonlinearVariationalProblem(F, w, bcs=bcs_w, J=J)
-
-                    solver = NonlinearVariationalSolver(
-                        problem,
-                        solver_parameters={
-                            "snes_type": "newtonls",
-                            "snes_linesearch_type": "bt",
-                            "snes_rtol": 1.0e-8,
-                            "snes_atol": 1.0e-10,
-                            "snes_max_it": 50,
-
-                            "mat_type": "aij",
-                            "ksp_type": "preonly",
-                            "pc_type": "lu",
-                            "pc_factor_mat_solver_type": "mumps",
-
-                            "snes_monitor": None,
-                            "ksp_monitor_true_residual": None,
-                            "ksp_error_if_not_converged": None,
-                        },
-                    )
-
-                    solver.solve()
-                    if theta == 0:
-                        uvec_out.assign(uvec)
-                        w.sub(0).assign(uvec_out)
-                    else:
-                        uvec_out.assign(w.sub(0))
-
-                    u_prev.assign(uvec_out)
-
-                print("Solving thickness evolution now...")
-
-                ubar = Function(VVbar, name="u_bar")
-
-                ubar.project(uvec_out, bcs=bcs_ubar)
-
-                ux_bar, uy_bar = split(ubar)
-
-                vel = as_vector([ux_bar, uy_bar])
-                vnorm = sqrt(dot(vel, vel) + 1e-10)
-                h = CellDiameter(mesh)
-                mu_art = 0.1 * h * vnorm
-
-                F = (
-                    thick_new * phi * dx \
-                    - thick * phi * dx \
-                    + dt * (ux_bar * thick_new).dx(0) * phi * dx
-                    + dt * (uy_bar * thick_new).dx(1) * phi * dx
-                    - dt * (a_s - a_b) * phi * dx
-                    # Artifical viscosity
-                    + dt * mu_art * dot(grad(thick_new), grad(phi)) * dx
+                        "snes_monitor": None,
+                        "ksp_monitor_true_residual": None,
+                        "ksp_error_if_not_converged": None,
+                    },
                 )
 
-                solve(lhs(F) == rhs(F), H)
-                thick.assign(H)
-                thick.dat.data[:] = np.maximum(thick.dat.data, 10.0)
+                solver.solve()
+                if theta_out == 0:
+                    uvec_out.assign(uvec)
+                    w.sub(0).assign(uvec_out)
+                else:
+                    uvec_out.assign(w.sub(0))
 
-                zb_float = -rhoi / rhow * thick
-                zb.interpolate(max_value(bed, zb_float))
-                zs.interpolate(zb + thick)
+                u_prev.assign(uvec_out)
 
-                mesh.coordinates.interpolate(as_vector([xref, yref, zb + sigmaref * thick]))
-                print("Finished solving thickness evolution...")
-                print("Year: ", (i+1)*dt)
+            print("Solving thickness evolution now...")
 
-                t = (i + 1) * dt
+            ubar = Function(VVbar, name="u_bar")
 
-                if abs(t % 50) < 1.0e-10 or abs((t % 50) - 50) < 1.0e-10:
-                    ux_out, uy_out = split(uvec_out)
-                    uout.interpolate(as_vector([ux_out, uy_out, 0.0]))
-                    grounded_out = Function(Vbar, name="grounded")
-                    grounded_out.interpolate(grounded)
-                    outfile.write(uout, thick, zs, zb, grounded_out, time=t)
+            ubar.project(uvec_out, bcs=bcs_ubar)
+
+            ux_bar, uy_bar = split(ubar)
+
+            vel = as_vector([ux_bar, uy_bar])
+            vnorm = sqrt(dot(vel, vel) + 1e-10)
+            h = CellDiameter(mesh)
+            mu_art = 0.1 * h * vnorm
+
+            F = (
+                thick_new * phi * dx \
+                - thick * phi * dx \
+                + dt * (ux_bar * thick_new).dx(0) * phi * dx
+                + dt * (uy_bar * thick_new).dx(1) * phi * dx
+                - dt * (a_s - a_b) * phi * dx
+                # Artifical viscosity
+                + dt * mu_art * dot(grad(thick_new), grad(phi)) * dx
+            )
+
+            solve(lhs(F) == rhs(F), H)
+            thick.assign(H)
+            thick.dat.data[:] = np.maximum(thick.dat.data, 10.0)
+
+            zb_float = -rhoi / rhow * thick
+            zb.interpolate(max_value(bed, zb_float))
+            zs.interpolate(zb + thick)
+
+            mesh.coordinates.interpolate(as_vector([xref, yref, zb + sigmaref * thick]))
+            print("Finished solving thickness evolution...")
+            print("Year: ", (i+1)*dt)
+
+            t = (i + 1) * dt
+
+            if abs(t % 50) < 1.0e-10 or abs((t % 50) - 50) < 1.0e-10:
+                ux_out, uy_out = split(uvec_out)
+                uout.interpolate(as_vector([ux_out, uy_out, 0.0]))
+                grounded_out.interpolate(1.0 - tanh((zb - bed) / delta_zb))
+                outfile.write(uout, thick, zs, zb, grounded_out, time=t)
+                restart_file = f"Simulations/restart_theta{theta_out:g}_dt{dt:g}_t{t:g}.h5"
+                save_restart(restart_file, t, i+1, dt, theta_out)

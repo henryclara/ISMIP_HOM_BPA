568132b4e0ab0f93296f014cd5c1feee16a86596
henryclara <clara.henry@bas.ac.uk>
2026-05-18 12:07:35 +0100

    Better structure

--- code_BPA/spaces.py
+++ code_BPA/spaces.py
@@ -1,23 +1,40 @@
 from firedrake import *
 
+# -----------------------------
+# Elements
+# -----------------------------
+
 # Horizontal and vertical elements
 horiz = FiniteElement("CG", triangle, 1)
 vert  = FiniteElement("CG", interval, 1)
 
-# Scalar tensor-product element: CG1 (horizontal) x CG1 (vertical)
 scalar_elt = TensorProductElement(horiz, vert)
-V = FunctionSpace(mesh, scalar_elt)
 
 vector_elt = VectorElement(scalar_elt, dim=2)
+
+vector3_elt = VectorElement(scalar_elt, dim=3)
+
+# -----------------------------
+# Function spaces
+# -----------------------------
+
+V = FunctionSpace(mesh, scalar_elt)
 VV = FunctionSpace(mesh, vector_elt)
 
-# Vector-valued version of the same tensor-product space
-vector3_elt = VectorElement(scalar_elt, dim=3)
 VV3 = FunctionSpace(mesh, vector3_elt)
 uout = Function(VV3, name="uout")
 
 Vbar = FunctionSpace(mesh, "CG", 1, vfamily="R", vdegree=0)
 VVbar = VectorFunctionSpace(mesh, "CG", 1, vfamily="R", vdegree=0, dim=2)
+
+H = Function(Vbar)
+u_prev = Function(VV)
+
+uvec_out = Function(VV, name="uvec")
+
+# -----------------------------
+# Mixed spaces
+# -----------------------------
 
 W = VV * VVbar * Vbar
 w = Function(W)
@@ -26,13 +43,17 @@ uvec, u_s, q = split(w)
 ux, uy = split(uvec)
 ux_s, uy_s = split(u_s)
 
+# -----------------------------
+# Test functions
+# -----------------------------
+
 vvect, eta, r = TestFunctions(W)
 v1, v2 = split(vvect)
 
-uvec_out = Function(VV, name="uvec")
-
 phi = TestFunction(Vbar)
-thick_new = TrialFunction(Vbar)
-H = Function(Vbar)
 
-u_prev = Function(VV)
+# -----------------------------
+# Trial functions
+# -----------------------------
+
+thick_new = TrialFunction(Vbar)

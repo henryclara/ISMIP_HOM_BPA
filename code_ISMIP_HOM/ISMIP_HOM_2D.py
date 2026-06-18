d1da5641e600e961ac3bc32a4e9a74c7f9a5032b
henryclara <clara.henry@bas.ac.uk>
2026-06-18 08:19:43 +0100

    partly converted to 2D

--- code_ISMIP_HOM/ISMIP_HOM_2D.py
+++ code_ISMIP_HOM/ISMIP_HOM_2D.py
@@ -5,63 +5,47 @@ import numpy as np
 import time
 
 Lx = 80000.0
-Ly = 80000.0
 nz = 10
 
-base = PeriodicRectangleMesh(50, 50, Lx, Ly)
-
-nz = 10
+base = PeriodicIntervalMesh(50, Lx)
 mesh = ExtrudedMesh(base, layers=nz, layer_height=1.0 / nz)
-x, y, sigma = SpatialCoordinate(mesh)
+
+x, sigma = SpatialCoordinate(mesh)
 
 Xref = Function(mesh.coordinates.function_space(), name="Xref")
 Xref.interpolate(SpatialCoordinate(mesh))
-xref, yref, sigmaref = split(Xref)
+xref, sigmaref = split(Xref)
 
 # Horizontal and vertical elements
-horiz = FiniteElement("CG", triangle, 1)
+horiz = FiniteElement("CG", interval, 1)
 vert  = FiniteElement("CG", interval, 1)
-
-# Scalar tensor-product element: CG1 (horizontal) x CG1 (vertical)
 scalar_elt = TensorProductElement(horiz, vert)
 V = FunctionSpace(mesh, scalar_elt)
 
-vector_elt = VectorElement(scalar_elt, dim=2)
-VV = FunctionSpace(mesh, vector_elt)
+U = FunctionSpace(mesh, scalar_elt)
 
 # Vector-valued version of the same tensor-product space
-vector3_elt = VectorElement(scalar_elt, dim=3)
-VV3 = FunctionSpace(mesh, vector3_elt)
-uout = Function(VV3, name="uout")
+vector2_elt = VectorElement(scalar_elt, dim=2)
+VV2 = FunctionSpace(mesh, vector2_elt)
+uout = Function(VV2, name="uout")
 
 Vbar = FunctionSpace(mesh, "CG", 1, vfamily="R", vdegree=0)
-VVbar = VectorFunctionSpace(mesh, "CG", 1, vfamily="R", vdegree=0, dim=2)
+Ubar = FunctionSpace(mesh, "CG", 1, vfamily="R", vdegree=0)
 
-#W = VV * Vbar
-#w = Function(W)
-
-W = VV * VVbar * Vbar
+W = U * Ubar * Vbar
 w = Function(W)
 
-uvec, u_s, q = split(w)
-ux, uy = split(uvec)
-ux_s, uy_s = split(u_s)
+u, u_s, q = split(w)
+v, eta, r = TestFunctions(W)
 
-vvect, eta, r = TestFunctions(W)
-v1, v2 = split(vvect)
-
-#dw = TrialFunction(W)
-#vvect, r = TestFunctions(W)
-#v1, v2 = split(vvect)
-
-uvec_out = Function(VV, name="uvec")
+u_out = Function(U, name="u")
 
 phi = TestFunction(Vbar)
 thick_new = TrialFunction(Vbar)
 H = Function(Vbar)
 
-u_prev = Function(VV)
-u_prev_ts = Function(VV)
+u_prev = Function(U)
+u_prev_ts = Function(U)
 
 yearinsec = 365.25 * 24 * 60 * 60
 A = Constant(1.0e-25 * yearinsec * 1.0e18)
@@ -75,21 +59,15 @@ rhow = 1028.0/(1.0e6*yearinsec**2)
 
 zs = Function(Vbar, name="zs").interpolate(0.0)
 zb = Function(Vbar, name="zb").interpolate(zs - 1000.0 \
-             + 500.0 * sin(omega * x) * sin(omega * y))
+             + 500.0 * sin(omega * x))
 
 thick = Function(Vbar, name="thick").interpolate(zs - zb)
 
-mesh.coordinates.interpolate(as_vector([xref, yref, zb + sigmaref * thick]))
+mesh.coordinates.interpolate(as_vector([xref, zb + sigmaref * thick]))
 eps = Constant(1e-6) # Constant(1e-10)
 
-def viscosity(ux, uy, n=1):
-    '''
-    Double check this against the derivation
-    '''
-    eps_e2 = (ux.dx(0)**2 + uy.dx(1)**2 + ux.dx(0) * uy.dx(1) \
-              + 0.25 * (ux.dx(1) + uy.dx(0))**2 + 0.25 * ux.dx(2)**2 \
-              + 0.25 * uy.dx(2)**2)
-
+def viscosity(u, n=3):
+    eps_e2 = u.dx(0)**2 + 0.25 * u.dx(1)**2
     mu = 0.5 * A**(-1.0 / n) * (eps_e2 + eps**2)**((1.0 - n) / (2.0 * n))
     return mu
 
@@ -98,7 +76,7 @@ ns = [3] #np.linspace(1, 3, 11)
 
 # Basal friction field
 beta2 = Function(Vbar, name="beta2")
-beta2.interpolate(1000.0 * (1.0 + sin(2.0*np.pi*x/Lx) * sin(2.0*np.pi*y/Lx)))
+beta2.interpolate(1000.0 * (1.0 + sin(2.0*np.pi*x/Lx)))
 
 a_s = 0.0
 a_b = 0.0
@@ -120,20 +98,17 @@ for dt in dts:
         num_TS = int(T / dt)
 
         zs.interpolate(0.0)
-        zb.interpolate(zs - 1000.0 + 500.0 * sin(omega * x) * sin(omega * y))
+        zb.interpolate(zs - 1000.0 + 500.0 * sin(omega * x))
         thick.interpolate(zs - zb)
-        mesh.coordinates.interpolate(as_vector([xref, yref, zb + sigmaref * thick]))
+        mesh.coordinates.interpolate(as_vector([xref, zb + sigmaref * thick]))
 
-        u_init = as_vector([10.0 * (1.0 + 0.01 * sin(2.0*pi*x/Lx) * sin(2.0*pi*y/Ly)), \
-                            10.0 * (0.01 * sin(2.0*pi*x/Lx) * sin(2.0*pi*y/Ly)),])
+        u_init = 10.0 * (1.0 + 0.01 * sin(2.0*pi*x/Lx))
 
-        w.sub(0).interpolate(u_init)   # initial guess for u
-        w.sub(1).interpolate(u_init)   # initial guess for u_s
+        w.sub(0).interpolate(u_init)
+        w.sub(1).interpolate(u_init)
 
         u0 = w.sub(0)
-        ux0, uy0 = split(u0)
-
-        w.sub(2).project(thick * (ux0.dx(0) + uy0.dx(1)))
+        w.sub(2).project(thick * u0.dx(0))
 
         u_prev.assign(0.0)
         u_prev_ts.assign(0.0)
@@ -152,85 +127,55 @@ for dt in dts:
                 #iter_sim=iter_sim+1
 
                 if theta_out == 0:
-                    uvec = Function(VV)
-                    uvec.interpolate(w.sub(0))   # use current mixed velocity guess if available
+                    u_solve = Function(U)
+                    u_solve.interpolate(w.sub(0))
 
-                    ux, uy = split(uvec)
+                    du = TrialFunction(U)
+                    v = TestFunction(U)
 
-                    du = TrialFunction(VV)
-                    vvect = TestFunction(VV)
-                    v1, v2 = split(vvect)
+                    u_for_form = u_solve
 
                 else:
-                    uvec, u_s, q = split(w)
-                    ux, uy = split(uvec)
-                    ux_s, uy_s = split(u_s)
+                    u, u_s, q = split(w)
 
                     dw = TrialFunction(W)
-                    vvect, eta, r = TestFunctions(W)
-                    v1, v2 = split(vvect)
+                    v, eta, r = TestFunctions(W)
 
-                mu = viscosity(ux, uy, n)
+                    u_for_form = u
+
+                mu = viscosity(u_for_form, n)
 
                 grad_zs_H = as_vector([zs.dx(0), zs.dx(1)])
                 surf = 1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)
 
-                #q.project(thick * (ux.dx(0) + uy.dx(1)))
+                mu = viscosity(u, n)
 
-                F = (4 * mu * ux.dx(0) + 2 * mu * uy.dx(1)) * v1.dx(0) * dx \
-                                + (mu * ux.dx(1) + mu * uy.dx(0)) * v1.dx(1) * dx \
-                                + mu * ux.dx(2) * v1.dx(2) * dx
-                            
-                F += (4 * mu * uy.dx(1) + 2 * mu * ux.dx(0)) * v2.dx(1) * dx \
-                                + (mu * uy.dx(0) + mu * ux.dx(1)) * v2.dx(0) * dx \
-                                + mu * uy.dx(2) * v2.dx(2) * dx
-                            
-                F += beta2 * dot(uvec, vvect) * ds_b
-                
+                F = (
+                    4 * mu * u.dx(0) * v.dx(0) * dx
+                    + mu * u.dx(1) * v.dx(1) * dx
+                    + beta2 * u * v * ds_b
+                )
+
                 if theta_out != 0:
-                    
-                    # Second term...
-                    F += theta * rhoi * g * dt * (ux_s * zs.dx(0) + uy_s * zs.dx(1)) * (v1.dx(0) + v2.dx(1)) * dx
-                    
-                    # Third term
-                    F += theta * rhoi * g * np.cos(psi) * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * q * (v1.dx(0) + v2.dx(1)) * dx
-                
-                    # Fourth term
-                    F += 0.5 * theta * g * np.cos(psi) * dt * (ux * (zs * zs).dx(0) + uy * (zs * zs).dx(1)) * (v1.dx(0) + v2.dx(1)) * (1 / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)) * ds_t
+                    F += theta * rhoi * g * dt * (u_s * zs.dx(0)) * v.dx(0) * dx
 
-                    # Fifth term
-                    F += theta * rhoi * g * np.cos(psi) * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) * zs * ((v1.dx(0) + v2.dx(1)) / sqrt(1 + zs.dx(0)**2 + zs.dx(1)**2)) * q * ds_t
-                
-                    # The constraints:
-                    F += dot(u_s - uvec, eta) * ds_t
-                    F += r * q * dx - r * thick * (ux.dx(0) + uy.dx(1)) * dx
+                    F += theta * rhoi * g * np.cos(psi) * dt \
+                        * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) \
+                        * q * v.dx(0) * dx
 
-                # The stabilisation terms (the version where we assume that test function gradients are depth independent)
-                '''
-                if theta_out != 0:
-                    F += theta * rhoi * g * np.cos(psi) * dt * thick * (ux * zs.dx(0) + uy * zs.dx(1)) \
-                                    * (v1.dx(0) + v2.dx(1)) * surf * ds_t
+                    F += 0.5 * theta * g * np.cos(psi) * dt \
+                        * u * (zs * zs).dx(0) * v.dx(0) \
+                        * (1 / sqrt(1 + zs.dx(0)**2)) * ds_t
 
-                    F += theta * rhoi * g * np.cos(psi) * dt * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) \
-                                    * q * (v1.dx(0) + v2.dx(1)) * dx
+                    F += theta * rhoi * g * np.cos(psi) * dt \
+                        * (((1 - zeta) * rhoi - rhow)/(rhoi - rhow)) \
+                        * zs * (v.dx(0) / sqrt(1 + zs.dx(0)**2)) * q * ds_t
 
-                    div_h = ux.dx(0) + uy.dx(1)
+                    F += (u_s - u) * eta * ds_t
+                    F += r * q * dx - r * thick * u.dx(0) * dx
 
-                    F += r * q * dx - r * thick * div_h * dx
-
-                '''
-
-                F -= rhoi * g * np.cos(psi) * zs * (v1.dx(0) + v2.dx(1)) * dx \
-                            - rhoi * g * np.sin(psi) * v1 * dx
-                    
-                    # Ignore accumulation for now.
-                    #- theta * rhoi * g * dt * (a_s - a_b) * zb.dx(0) * v1 * dx \
-                    #- theta * rhoi * g * dt * (a_s - a_b) * zb.dx(1) * v2 * dx \
-                    #+ theta * rhoi * g * dt * thick * (a_s - a_b) * v1.dx(0) * dx \
-                    #+ theta * rhoi * g * dt * thick * (a_s - a_b) * v2.dx(1) * dx
-
-                #J = derivative(F, uvec, du)
-                #problem = NonlinearVariationalProblem(F, uvec, J=J)
+                F -= rhoi * g * np.cos(psi) * zs * v.dx(0) * dx \
+                    - rhoi * g * np.sin(psi) * v * dx
 
                 if theta_out == 0:
                     J = derivative(F, uvec, du)
@@ -271,22 +216,16 @@ for dt in dts:
             print("Solving thickness evolution now...")
 
             ubar = Function(VVbar, name="u_bar")
+            ubar.project(u_out)
 
-            # This needs improvement. We need to solve a weak form problem here.
-            ubar.project(uvec_out)
-            ux_bar, uy_bar = split(ubar)
-
-            vel = as_vector([ux_bar, uy_bar])
-            vnorm = sqrt(dot(vel, vel) + 1e-10)
+            vnorm = sqrt(ubar**2 + 1e-10)
             h = CellDiameter(mesh)
             mu_art = 0.1 * h * vnorm
 
             F = (
-                thick_new * phi * dx \
-                - thick * phi * dx \
-                + dt * (ux_bar * thick_new).dx(0) * phi * dx
-                + dt * (uy_bar * thick_new).dx(1) * phi * dx
-                # Artifical viscosity
+                thick_new * phi * dx
+                - thick * phi * dx
+                + dt * (ubar * thick_new).dx(0) * phi * dx
                 + dt * mu_art * dot(grad(thick_new), grad(phi)) * dx
             )
 
